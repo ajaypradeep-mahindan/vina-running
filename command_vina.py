78b26d2e05202ea292e91834a324784b2e42980b
Ajay Pradeep <79718383+ajaypradeep-mahindan@users.noreply.github.com>
2024-01-11 14:40:59 +0530

    multiple-ligands-vina.py

--- command_vina.py
+++ command_vina.py
@@ -1,28 +1,37 @@
+#this program is to run the autodock vina to run the multiple ligand docking 
+
+
+
+
 import os
 
 def run_autodock_vina(vina_executable, receptor_pdbqt, ligand_pdbqt, output_dir, config):
-    ligand_name = os.path.splitext(os.path.basename(ligand_pdbqt))[0]
-    output_file = os.path.join(output_dir, f"{ligand_name}_result.pdbqt")
-    log_file = os.path.join(output_dir, f"{ligand_name}_log.txt")
+    ligand_name = os.path.splitext(os.path.basename(ligand_pdbqt))[0]#to get the ligand filename 
+    output_file = os.path.join(output_dir, f"{ligand_name}_result.pdbqt")#to save the output file corresopnding to each ligand 
+    log_file = os.path.join(output_dir, f"{ligand_name}_log.txt")# to save the log file of each binding scores corresponding to each ligand 
     
     command = f"{vina_executable} --receptor {receptor_pdbqt} --ligand {ligand_pdbqt} --out {output_file} --config {config} --log {log_file}"
     os.system(command)
 
 def run_autodock_vina_for_multiple_ligands(vina_executable, receptor_pdbqt, ligand_dir, output_dir, config):
     if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+        os.makedirs(output_dir)# creates output folder if it doesnt exist
 
     ligands = [f for f in os.listdir(ligand_dir) if f.endswith(".pdbqt")]
+
+    if not os.path.exists(vina_executable) or not os.access(vina_executable, os.X_OK):
+        print("AutoDock Vina executable not found or not executable. Please install Vina and provide the correct path.") #to verify the installation exist 
+        exit()
 
     for ligand in ligands:
         ligand_path = os.path.join(ligand_dir, ligand)
         run_autodock_vina(vina_executable, receptor_pdbqt, ligand_path, output_dir, config)
 
 if __name__ == "__main__":
-    vina_executable = "D:/runVina/vina.exe"  # Update with the path to your AutoDock Vina executable
-    receptor_pdbqt = "D:/runVina/4bfs.pdbqt"  # Update with the path to your receptor PDBQT file
-    config = "D:/runVina/config4.txt"  # Update with the path to your configuration file
-    ligand_dir = "D:/runVina/ligands"  # Update with the path to your ligands directory
-    output_dir = "D:/runVina/opf"  # Update with the desired output directory
+    vina_executable = "/path/to/vina"  #  path to your AutoDock Vina executable on Linux
+    receptor_pdbqt = "/path/to/receptor.pdbqt"  #  path to your receptor PDBQT file
+    config = "/path/to/config.txt"  #  path to your configuration file
+    ligand_dir = "/path/to/ligands"  # path to your ligands directory
+    output_dir = "/path/to/output"  # desired output directory
 
     run_autodock_vina_for_multiple_ligands(vina_executable, receptor_pdbqt, ligand_dir, output_dir, config)
